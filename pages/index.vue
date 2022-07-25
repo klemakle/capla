7a8f75b916d3fdc517527af2c2cbd0671a05413b
Kalidou <kalidou1309@gmail.com>
2022-07-25 22:46:12 +0000

    add shadow to certificate

--- pages/index.vue
+++ pages/index.vue
@@ -38,20 +38,24 @@
 
 <!-- delivrance certificat -->
 
-    <div class="flex flex-col-reverse md:flex-row py-4 px-4 justify-between items-center my-10 mx-auto md:mx-12 border-2 border-gray-200">
+    <div class=" bg-gray-50 flex flex-col-reverse md:flex-row py-4 px-4 justify-between items-center my-10 mx-auto md:mx-12 border-2 border-gray-200">
       <div id="certificat_text">
-        <p class="text-gray-500">
+        <p class="text-gray-500 text-3xl">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus asperiores sint nobis veritatis deserunt explicabo similique iusto, eveniet aut doloremque numquam quis itaque fugit voluptates incidunt facilis ut, voluptatum perferendis.
         </p>
       </div>
 
-      <div class="w-full ml-4">
+      <div class="w-full ml-4 relative mb-8 md:mb-0 mx-2">
+        <img src="../assets/images/degree/grid.png" class="absolute -top-10 -right-8 md:-top-12 md:-right-12 z-10 w-20 h-20 md:w-32 md:h-32" alt=""></img>
         <vueper-slides class="no-shadow h-full" bullets-outside transition-speed="250" :breakpoints="breakpoint" fixed-height="500px" :arrows="false">
-  <vueper-slide
-    v-for="(elem,i) in certificat_photos"
-    :key="i"
-    :image="elem.image" />
-</vueper-slides>
+          <vueper-slide
+            v-for="(elem,i) in certificat_photos"
+            :key="i"
+            :image="elem.image"
+            class="rounded-md drop-shadow-lg"
+             />
+          </vueper-slides>
+          <img src="../assets/images/degree/grid.png" class="absolute bottom-0 -left-4 md:-bottom-2 md:-left-10 z-0 w-20 h-20 md:w-32 md:h-32" alt="">
       </div>
     </div>
 
