5a5616c27fb4d7f3cb5a27bb33bc5a1343e8b1df
Kalidou <kalidou1309@gmail.com>
2022-07-27 15:50:53 +0000

    new build

--- dist/_nuxt/c89422b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{469:function(n,e,t){"use strict";t.r(e);var l={layout:"navbar"},r=t(9),component=Object(r.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  Services\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
