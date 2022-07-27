304a24b9fb1b183029377eebc23da34df694a8f9
Kalidou <kalidou1309@gmail.com>
2022-07-27 15:32:18 +0000

    services page

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    Services
+    <div><Services/></div>
+
+    <!-- modalités -->
+    <div><Modalites/> </div>
   </div>
 </template>
 
 <script>
+
+import Services from '~/components/landing/services.vue';
+import Offre from '~/components/landing/offre.vue'
+import Certificat from '~/components/landing/certificat.vue';
+import Modalites from '~/components/landing/modalites.vue';
 export default {
   layout:'navbar',
+  components:{Services, Offre, Certificat, Modalites},
 }
 </script>
