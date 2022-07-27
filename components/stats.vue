06896f3962e6654a6d9fb21d47a09cd06f13af93
Kalidou <kalidou1309@gmail.com>
2022-07-27 15:40:34 +0000

    pour tout age

--- components/stats.vue
+++ components/stats.vue
@@ -48,11 +48,11 @@
              <div class="py-2 px-3 bg-white w-full md:w-1/5 rounded-lg my-4">
                 <div class="top-card flex flex-row justify-between mb-6">
                 <div class="px-2 py-2 w-1/4 bg-gray-50 self-start rounded-lg -mt-7">
-                    <img src="../assets/images/stats/woman.png" class="center" alt="promo_alt">
+                    <img src="../assets/images/stats/age.png" class="center" alt="promo_alt">
                 </div>
-                <div class="flex flex-col text-gray-500 justify-start">
-                    <p class="">Femmes</p>
-                    <p id="promo" class="text-gray-500 self-end text-base">300</p>
+                <div class="flex flex-col text-gray-400 justify-start">
+                    <p>Pour tous</p>
+                    <p id="promo" class="text-gray-500 self-end text-base">de 8 à 65 ans</p>
                 </div>
                 </div>
                 <p class="self-end mt-4 text-gray-500">Depuis sa création</p>
