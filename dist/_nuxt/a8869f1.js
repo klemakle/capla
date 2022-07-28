75052a0b54ad119e4ad7e51e9fce5de23d933a71
Kalidou <kalidou1309@gmail.com>
2022-07-28 02:18:19 +0000

    new build

---
+++ dist/_nuxt/a8869f1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{472:function(n,t,e){"use strict";e.r(t);var l={layout:"navbar"},c=e(10),component=Object(c.a)(l,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",[n._v("\n  Activity\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ dist/_nuxt/static/1658974670/activity/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/activity", {data:[{}],fetch:{},mutations:void 0});
