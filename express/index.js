db0d84a20e1be9fd2d368749b60771186253b0cf
Kalidou <kalidou1309@gmail.com>
2022-08-03 04:05:59 +0000

    test write file function

--- express/index.js
+++ express/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+const { callbackify } = require('util');
 var session = require('./session.json');
 
 const app = express();
@@ -9,7 +10,17 @@ app.get('/hello', (req, res) => {
 })
 
 app.put('/new-session', (req, res) => {
-
+    const { session, debut } = req.body;
+    fs.readFile('./session.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            obj = JSON.parse(data)
+            obj.push({ session: 'coucou', debut: 'demain' });
+            json = JSON.stringify(obj);
+            fs.writeFile('./session.json', json, 'utf8', callback)
+        }
+    })
 })
 
 module.exports = app
