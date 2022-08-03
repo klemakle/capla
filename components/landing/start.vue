4e9017752307df61fd693649f43f7e8af53c8aeb
Kalidou <kalidou1309@gmail.com>
2022-08-03 03:18:28 +0000

    separate files

---
+++ components/landing/start.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="-mx-2 md:-mx-4 flex justify-center items-center md:justify-center my-12 bg-indigo-500 px-4 md:px-8 py-14">
+            <div class="flex flex-col justify-center items-center">
+                <div class="text-lg md:text-xl text-indigo-200 font-thin items-center mb-8 font-russo">
+                    Prochaine rentrée
+                </div>
+                <div class="text-indigo-50 my-2 font-bold text-2xl md:text-5xl font-russo ">
+                    Session d'été
+                </div>
+
+                <div class="text-indigo-50 my-2 font-bold text-2xl md:text-5xl font-russo ">
+                    17 Juillet 2022
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'start'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -13,22 +13,8 @@
     <div><Offre/></div>
 
     <!-- prochaine rentrée -->
-    <div class="-mx-2 md:-mx-4 flex justify-between items-center md:justify-center my-12 bg-indigo-500 px-4 md:px-8 py-14">
-      <div class="items-center"><span class="text-indigo-500 text-5xl material-symbols-outlined" style="transform:rotate(180deg);">double_arrow</span></div>
-      <div class="flex flex-col justify-center items-center">
-        <div class="text-lg md:text-xl text-indigo-200 font-thin items-center mt-2 mb-8 font-russo">
-          Prochaine rentrée
-        </div>
-        <div class="text-indigo-50 my-2 font-bold text-2xl md:text-5xl font-russo ">
-          Session été
-        </div>
-
-        <div class="text-indigo-50 my-2 font-bold text-2xl md:text-5xl font-russo ">
-          17 Juillet 2022
-        </div>
-      </div>
-      <div><span class="text-indigo-500 text-5xl material-symbols-outlined">double_arrow</span></div>
-    </div>
+    <div><Start/></div>
+    
     <!-- delivrance certificat -->
     <div><Certificat/></div>
 
@@ -50,7 +36,8 @@
 import { VueperSlides, VueperSlide } from 'vueperslides';
 import Presentation from '../components/landing/presentation.vue'
 import Services from '../components/landing/services.vue';
-import Offre from '../components/landing/offre.vue'
+import Offre from '../components/landing/offre.vue';
+import Start from '../components/landing/start.vue';
 import Certificat from '../components/landing/certificat.vue';
 import Modalites from '../components/landing/modalites.vue';
 import Temoignage from '../components/landing/temoignage.vue';
@@ -67,7 +54,7 @@ export default {
     name: 'home',
     mode: 'out-in'
   },
-  components:{VueperSlides, VueperSlide, Presentation,Services, Offre, Certificat, Modalites,Temoignage, Partner},
+  components:{VueperSlides, VueperSlide, Presentation,Services, Offre, Start,Certificat, Modalites,Temoignage, Partner},
 }
 </script>
 
