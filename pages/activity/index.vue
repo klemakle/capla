25869f87cfe85f08b9b119a16d5c7b5989daf070
Kalidou <kalidou1309@gmail.com>
2022-08-13 00:08:21 +0000

    add vue slider on activity

--- pages/activity/index.vue
+++ pages/activity/index.vue
@@ -5,8 +5,9 @@
         v-for="(slide, i) in slides"
         :key="i"
         :image="slide.image"
+        :content="''" 
         :video="slide.video"
-        :content="''" />
+        />
     </vueper-slides>
   </div>
 </template>
@@ -38,10 +39,9 @@ export default {
             content: 'This Youtube video has params in the URL and extra attributes on the iframe.',
             image: 'https://i.ytimg.com/vi_webp/ehJg_OlcjpE/maxresdefault.webp',
             video: {
-              url: 'https://www.youtube.com/embed/ehJg_OlcjpE?rel=0&showinfo=0&controls=0&fs=0&modestbranding=1&color=white&iv_load_policy=3&autohide=1&enablejsapi=1',
-              props: {
-                allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-              }
+              // url: 'https://www.youtube.com/embed/ehJg_OlcjpE?rel=0&showinfo=0&controls=0&fs=0&modestbranding=1&color=white&iv_load_policy=3&autohide=1&enablejsapi=1',
+              webm: `https://www.youtube.com/watch?v=RXascZIIuQE`,
+               props: { autoplay: true, loop: true, controls: false, muted: true }
             }
           },
           {
@@ -49,11 +49,9 @@ export default {
             content: 'This video can\'t be interacted with.',
             image: 'https://i.ytimg.com/vi/2sr9MGkkeks/maxresdefault.jpg',
             video: {
-              url: 'https://www.youtube.com/embed/2sr9MGkkeks?controls=0&fs=0&modestbranding=1&color=white&iv_load_policy=3&autohide=1&enablejsapi=1',
-              props: {
-                allow: 'autoplay'
-              },
-              pointerEvents: false
+              // url: 'https://www.youtube.com/embed/2sr9MGkkeks?controls=0&fs=0&modestbranding=1&color=white&iv_load_policy=3&autohide=1&enablejsapi=1',
+              webm: `https://www.youtube.com/watch?v=RXascZIIuQE`,
+               props: { autoplay: true, loop: true, controls: false, muted: true }
             }
           }
 ]
